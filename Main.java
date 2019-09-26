357481120fa8db714d005f993ee66ec485cbd654
mmerabi <merabimichael@gmail.com>
2019-09-25 17:54:27 -0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,24 +3,35 @@ import java.util.*;
 
 
 public class Main {
+    int firstN;
+    int inputInt;
+
     public static void main(String[] args) {
 
         System.out.println("The Stablematching program by Michael Merabi is now starting");
-        fileReader();
+        fileReader("./src/input1.txt");
 
     }
 
+    public static int[][] fileReader(String file) {
+        int[][] array = null;
+        String inputString;
 
-    public static void fileReader() {
         try {
-            BufferedReader br = new BufferedReader(new FileReader("./src/input1.txt"));
 
-            
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            inputString = br.readLine();
+
+            while (( inputString = br.readLine()) != null) {
+
+            }
+
         }
         catch (Exception filereader){
             System.out.println("Error while pulling file");
             filereader.printStackTrace();
         }
+        return array;
     }
 
 
