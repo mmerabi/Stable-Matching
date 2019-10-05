7a7bb288c58c0899b155c7358564f81aa313caf7
mmerabi <merabimichael@gmail.com>
2019-10-05 14:00:56 -0700

    Add files via upload
    New approach

--- Main.java
+++ Main.java
@@ -1,11 +1,5 @@
 import java.io.*;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.stream.Stream;
-import java.util.Scanner;
-import java.util.Arrays;
+import java.util.*;
 
 public class Main {
 
@@ -13,160 +7,77 @@ public class Main {
 
         System.out.println("The Stablematching program by Michael Merabi is now starting");
 
-        //Create new double array and read in all the information using people/person classes
-        people people_arr = new people();
-        people_arr = fileReader("./src/input1.txt");
+        List<List<Integer>> people_arr = fileReader("./src/input1.txt");
+
+
 
         //run the solution to see if there is a possible matching
-        boolean solution = Solve(people_arr);
-
-        //Check to see how many solutions there is
-        if (solution == true){
-            int solutioncount = 1;
-
+        int solution = Solve(people_arr);
+        if (solution == 0) {
+            System.out.println("NO possible matching");
+        } else {
+            System.out.println("YES, there are " + solution + "possible solutions");
         }
     }
 
 
     //Method to take in double array and fill with preferences then return back to Main
-    private static people fileReader(String file) {
+    public static List<List<Integer>> fileReader(String file) {
 
         //number of people = n
-        int n;
-        int rows;
-        int columns;
-        int row_index = 0;
-        int column_index = 0;
+        int n = 0;
+        List<List<Integer>> matching_array = null;
 
         try { //pulling in data from the file
             Scanner scanner = new Scanner(new File(file));
             n = scanner.nextInt();
-            rows = n;
-            columns = n - 1;
-            int[][] matching_array = new int[rows][columns];
+            System.out.print(n);
+
+            //first add n
+            List<Integer> placeholder = null;
+            placeholder.add(n);
+            matching_array.add(placeholder);
 
             //Checking to make sure correct parsing
             System.out.println("The number of people is " + n);
-            System.out.println("The number of rows is " + rows);
-            System.out.println("The number of columns is " + columns);
 
-            //iterator to go through and fill the arrays
+            //iterator to go through and fill the lists
             while (scanner.hasNextLine()) {
-                matching_array[row_index][column_index] = scanner.nextInt();
-
-                column_index += 1;
-
-                if (column_index == columns) {
-                    row_index += 1;
-                    column_index = 0;
-                    if (row_index == rows) {
-                        break;
-                    }
+                List<Integer> preferences = null;
+                for (int i = 0; i < n-1; i++) {
+                    preferences.add(scanner.nextInt());
                 }
+                matching_array.add(preferences);
             }
             scanner.close();
-            System.out.println(Arrays.deepToString(matching_array));
+            matching_array.listIterator(); //sanity check
 
-            people preferences = new people();
-            preferences.setRows(rows);
-            preferences.setColumns(columns);
-            preferences.setN(n);
-            preferences.setArray(matching_array);
 
-            //Return the double array
-            return preferences;
+            //Return the List of lists
+            return matching_array;
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        return (new people());
+        return matching_array;
+    }
+
+
+    //create all possible permutations of N
+    public List<List<Integer>> permutations(int n){
+        List<List<Integer>> permutations = null;
+
+
+
+        return permutations;
     }
 
 
     //Method to solve the preferences array and return the amount of solutions
-    private static boolean Solve(people preferences) {
-        boolean solution = false;
-
+    private static int Solve( List<List<Integer>> people_arr) {
+        int solution = 0;
 
 
         return solution;
     }
-
 }
 
-
-
-
-
-//individual person class
-class person{
-    private int[] preferences;
-    private int currentProposal;
-    private boolean taken;
-
-
-    public boolean getTaken(){
-        return this.taken;
-    }
-
-    public int getFirstPreference(){
-        return this.preferences[0];
-    }
-
-
-    public void setPreferences(int[] arr){
-        this.preferences=new int[arr.length];
-        this.preferences=arr;
-    }
-
-    public void setTaken(boolean status){ this.taken=status; }
-
-    public void proposeTo(){
-        return;
-    }
-}
-
-//array of people class, used for sum total program
-class people{
-    private int rows;
-    private int columns;
-    private int n;
-    private int[][] roommates;
-
-    public int getN(){
-        return this.n;
-    }
-
-    public void setN(int n){
-        this.n=n;
-    }
-
-    public int getRows() {
-        return this.rows;
-    }
-
-    public void setRows(int row) {
-        this.rows=row;
-    }
-
-    public int getColumns() {
-        return this.columns;
-    }
-
-    public void setColumns(int column) {
-        this.columns=column;
-    }
-
-    public int[][] getArray() {
-        return this.roommates;
-    }
-
-    public void setArray(int[][] arr) {
-        this.roommates=arr;
-    }
-
-    public int[] getPerson(int index){
-        System.out.println(this.roommates[index]);
-        return this.roommates[index];
-    }
-
-}
